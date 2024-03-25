47c08cbe9ac259ef12fd81dcb7c12134720b233a
rstulug <rstulug@gmail.com>
2024-03-25 21:38:15 +0300

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import Results from "./pages/Results";
+import UserPage from "./pages/UserPage";
+import Signup from "./pages/Signup";
+import Recipe from "./pages/Recipe";
+import NotFound from "./ui/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "recipe/:recipeId", element: <Recipe /> },
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <Signup /> },
+      { path: "resetpassword", element: <ResetPassword /> },
+      { path: "user/:usernameSlug", element: <UserPage /> },
+      { path: "results", element: <Results /> },
+    ],
+  },
+]);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
