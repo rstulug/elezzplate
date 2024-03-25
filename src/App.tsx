a91bb1068847f190d75e7f36d2250cab53162526
rstulug <rstulug@gmail.com>
2024-03-25 22:13:55 +0300

    some adjusment 1

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 30,
+      staleTime: 1000 * 60 * 10,
     },
   },
 });
